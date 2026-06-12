09f42b2e0d8f7679a65923547fb5c1de303a7be7
JoselitoMoura <joselitodemourabarbosa>
2026-06-12 18:09:45 +0100

    Alterações no arquivo pom.xml para criação das dependências e indentação do arquivo.

---
+++ src/main/java/pt/uc00605/pratica2/Pratica2Application.java
@@ -0,0 +1,13 @@
+package pt.uc00605.pratica2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pratica2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pratica2Application.class, args);
+	}
+
+}
